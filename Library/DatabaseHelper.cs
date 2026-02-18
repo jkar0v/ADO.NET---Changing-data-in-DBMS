58f47f0eaad6e4296afd2917f4499033c5c72abb
Jivko Karov <jikito2007@gmail.com>
2026-02-18 13:04:51 +0200

    Add project files.

---
+++ Library/DatabaseHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class DatabaseHelper
+    {
+        private string connectionString =
+           "Server=localhost\\SQLEXPRESS;Database=LibraryDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public DataTable GetBooks()
+        {
+            DataTable table = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Title, Author, Price FROM Books";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                adapter.Fill(table);
+            }
+
+            return table;
+        }
+
+        public void InsertBook(string title, string author, decimal price)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Books (Title, Author, Price) VALUES (@title, @author, @price)";
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.InsertCommand = new SqlCommand(query, conn);
+
+                adapter.InsertCommand.Parameters.AddWithValue("@title", title);
+                adapter.InsertCommand.Parameters.AddWithValue("@author", author);
+                adapter.InsertCommand.Parameters.AddWithValue("@price", price);
+
+                conn.Open();
+                adapter.InsertCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Library/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+        DatabaseHelper dbHelper = new DatabaseHelper();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string title = textBox1.Text;
+            string author = textBox2.Text;
+            decimal price = Convert.ToDecimal(textBox3.Text);
+
+            dbHelper.InsertBook(title, author, price);
+
+            dataGridView1.DataSource = dbHelper.GetBooks();
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dbHelper.GetBooks();
+        }
+    }
+}
